ea3df88efc77cb0a874cbc9c4fdc1386cd98d2a8
jiseob-lee <rg2000@jisblee.me>
2024-12-08 10:02:32 +0900

    GeoLite

--- src/main/java/com/rg/util/GeoLite2.java
+++ src/main/java/com/rg/util/GeoLite2.java
@@ -3,26 +3,35 @@ package com.rg.util;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Locale;
 
-import jakarta.servlet.http.HttpServletRequest;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
+import com.maxmind.db.CHMCache;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.AddressNotFoundException;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
 import com.maxmind.geoip2.model.CountryResponse;
 import com.maxmind.geoip2.record.Country;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class GeoLite2 {
+	
+	private static final Logger logger = LogManager.getLogger(GeoLite2.class);
 	
 	public static String getCountry(HttpServletRequest request, String ip) {
 		
-		String GeoLite2Path = "D:/GeoLite2/GeoLite2-Country.mmdb";
+		Country country = null;
+		
+		String GeoLite2Path = "E:/GeoLite2/GeoLite2-City.mmdb";
 		
 		// A File object pointing to your GeoIP2 or GeoLite2 database
 		if (new File("/home/ubuntu/GeoLite2").exists()) {
-			GeoLite2Path = "/home/ubuntu/GeoLite2/GeoLite2-Country.mmdb";
+			GeoLite2Path = "/home/ubuntu/GeoLite2/GeoLite2-City.mmdb";
 		}
 		
 		File database = new File(GeoLite2Path);
@@ -31,16 +40,20 @@ public class GeoLite2 {
 		// the object across lookups. The object is thread-safe.
 		DatabaseReader reader = null;
 		InetAddress ipAddress = null;
-		CountryResponse response = null;
+		CityResponse response = null;
 		
 		String ip2 = request.getHeader("X-Forwarded-For") == null ? request.getRemoteAddr() : request.getHeader("X-Forwarded-For");
 		
 		try {
 
-			reader = new DatabaseReader.Builder(database).build();
+			reader = new DatabaseReader.Builder(database).withCache(new CHMCache()).build();
 			
-			System.out.println("######################################## RemoteAddr 1 : " + request.getRemoteAddr());
-			System.out.println("######################################## RemoteAddr 1-1 : " + request.getHeader("X-Forwarded-For"));
+			//WebServiceClient client = new WebServiceClient.Builder(171734, "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")
+				    //.build();
+			
+			//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
+			logger.debug("######################################## RemoteAddr 1 : " + request.getRemoteAddr());
+			logger.debug("######################################## RemoteAddr 1-1 : " + request.getHeader("X-Forwarded-For"));
 
 			if (ip == null || "".equals(ip)) {
 				ipAddress = "127.0.0.1".equals(ip2) || "0:0:0:0:0:0:0:1".equals(ip2)
@@ -49,9 +62,12 @@ public class GeoLite2 {
 				ipAddress = InetAddress.getByName(ip);
 			}
 			
-			System.out.println("######################################## RemoteAddr 2 : " + ipAddress.toString());
+			logger.debug("######################################## RemoteAddr 2 : " + ipAddress.toString());
 			
-			response = reader.country(ipAddress);
+			response = reader.city(ipAddress);
+			//response = client.city(ipAddress);
+			
+			country = response.getCountry();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -60,12 +76,13 @@ public class GeoLite2 {
 			return "KR";
 		} catch (GeoIp2Exception e) {
 			e.printStackTrace();
+			return Locale.FRANCE.getISO3Country();
 		}
 
-		Country country = response.getCountry();
-		System.out.println(country.getIsoCode());            // 'US'
-		System.out.println(country.getName());               // 'United States'
-		System.out.println(country.getNames().get("zh-CN")); // '美国'
+		//Country country = response.getCountry();
+		logger.debug(country.getIsoCode());            // 'US'
+		logger.debug(country.getName());               // 'United States'
+		logger.debug(country.getNames().get("zh-CN")); // '美国'
 
 		return country.getIsoCode();
 	}
@@ -75,7 +92,8 @@ public class GeoLite2 {
 		
 		// A File object pointing to your GeoIP2 or GeoLite2 database
 		File database = new File("/home/ubuntu/GeoLite2/GeoLite2-Country.mmdb");
-		database = new File("E:/work/20240124 - modal/GeoLite2-Country.mmdb");
+		//database = new File("E:/work/20240124 - modal/GeoLite2-Country.mmdb");
+		database = new File("E:/GeoLite2/GeoLite2-Country.mmdb");
 		// This creates the DatabaseReader object. To improve performance, reuse
 		// the object across lookups. The object is thread-safe.
 		DatabaseReader reader = null;
@@ -97,9 +115,9 @@ public class GeoLite2 {
 		}
 
 		Country country = response.getCountry();
-		System.out.println(country.getIsoCode());            // 'US'
-		System.out.println(country.getName());               // 'United States'
-		System.out.println(country.getNames().get("zh-CN")); // '美国'
+		logger.debug(country.getIsoCode());            // 'US'
+		logger.debug(country.getName());               // 'United States'
+		logger.debug(country.getNames().get("zh-CN")); // '美国'
 		
 	}
 }
