5622390a84a478be19ecbc17eb9ad7f9a5ee8e3e
이지섭 <jiseob93@naver.com>
2025-06-18 07:33:56 +0900

    소스 포맷

--- src/main/java/com/rg/util/OSValidator.java
+++ src/main/java/com/rg/util/OSValidator.java
@@ -4,18 +4,18 @@ import org.junit.Test;
 
 public class OSValidator {
 
-    private static final String OS = System.getProperty("os.name").toLowerCase();
+	private static final String OS = System.getProperty("os.name").toLowerCase();
 
-    public static boolean isWindows() {
-        return OS.contains("win");
-    }
+	public static boolean isWindows() {
+		return OS.contains("win");
+	}
 
-    @Test
-    public void test() {
-        if (isWindows()) {
-            System.out.println("This is a Windows operating system.");
-        } else {
-            System.out.println("This is not a Windows operating system.");
-        }
-    }
+	@Test
+	public void test() {
+		if (isWindows()) {
+			System.out.println("This is a Windows operating system.");
+		} else {
+			System.out.println("This is not a Windows operating system.");
+		}
+	}
 }
